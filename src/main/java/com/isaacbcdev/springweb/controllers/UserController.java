e8d55f26789312406272ab273f1fc7a47d329e66
isaacbcdev <109006224+isaacbcdev@users.noreply.github.com>
2025-05-06 22:00:16 -0500

    thymeleaf lists and global Model Attribute test

--- src/main/java/com/isaacbcdev/springweb/controllers/UserController.java
+++ src/main/java/com/isaacbcdev/springweb/controllers/UserController.java
@@ -3,9 +3,13 @@ package com.isaacbcdev.springweb.controllers;
 import com.isaacbcdev.springweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -27,19 +31,45 @@ public class UserController {
 
         User user = new User("Isaac", "Coding");
         model.addAttribute("user", user);
-        model.addAttribute("age", "15");
-        return "details"; // This will return the "details" view - It is case-sensitive and should match the name of the HTML file in the 'templates' directory.
+        return "details"; // This will return the "details" view
+        // It is case-sensitive
+        // and should match the name of the HTML file in the 'templates' directory.
     }
 
     // This method will handle GET requests to the "/user" URL
     // and return the "user" view.
     @GetMapping("/user")
     public String user(Map<String, Object> model) {
-        // You can add attributes to the model to be used in the view
+        // You can add attributes to the model to be used in the view,
         // For example, model.put("user", user);
         // This will add a "user" attribute to the model with the value of the user object
         // You can also use the model to pass data to the view template (e.g., Thymeleaf, JSP, etc.) that will be rendered.
         model.put("user", "Isaac");
-        return "details"; // This will return the "user" view - It is case-sensitive and should match the name of the HTML file in the templates directory.
+        return "details"; // This will return the "user" view
+        // It is case-sensitive and should match the name of the HTML file in the 'templates' directory.
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        // This method will return a list of users
+        // You can use this method to get a list of users from the database or any other source
+//        List<User> users = Arrays.asList(
+//                new User("Isaac", "Coding", "isaac-coding@mail.com"),
+//                new User("John", "Doe"),
+//                new User("Jane", "Doe", "jane-doe@mail.com"));
+//        model.addAttribute("users", users);
+        model.addAttribute("title", "User List");
+        return "list"; // This will return the "list" view
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        // This method will return a list of users
+        // You can use this method to get a list of users from the database or any other source
+        List<User> users = Arrays.asList(
+                new User("Isaac", "Coding", "isaac-coding@mail.com"),
+                new User("John", "Doe"),
+                new User("Jane", "Doe", "jane-doe@mail.com"));
+        return users;
     }
 }
